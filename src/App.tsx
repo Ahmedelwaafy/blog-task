886c88deb7f897a00603b23fa555b8d9e3ee11d0
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2024-04-22 06:00:15 +0200

    deploy edit14

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import {
+  createBrowserRouter,
   Navigate,
   RouterProvider,
-  createBrowserRouter,
 } from "react-router-dom";
-import { setLang } from "./app/Features/MiscellaneousSlice.tsx";
 import { useAppDispatch } from "./app/reduxHooks.ts";
+import { setLang } from "./app/Features/MiscellaneousSlice.tsx";
 import Layout from "./components/Layout.tsx";
+
 function App() {
   const { i18n } = useTranslation("");
   const dispatchRedux = useAppDispatch();
   //!we use startsWith instead of ==== whereas some languages codes consist of 2 words as en-us but the language in the url always consists of one word
   useEffect(() => {
     dispatchRedux(setLang(i18n.language?.startsWith("ar") ? "ar" : "en"));
+
     document.documentElement.dir = i18n.dir();
     document.documentElement.lang = i18n.language?.startsWith("ar")
       ? "ar"
@@ -30,34 +32,33 @@ function App() {
     {
       path: `/${i18n.language?.startsWith("ar") ? "ar" : "en"}`,
       children: [
+        //!-------- Pages Layout--------
         {
           element: <Layout />,
           children: [
             {
               index: true,
-              //!react router built-in lazy loading component
+              lazy: () => import("./Pages/HomePage/HomePage.tsx"),
+            },
+
+            //!NotFound
+            {
+              path: "*",
               lazy: () => import("./Pages/HomePage/HomePage.tsx"),
             },
             {
-              path: "search",
-              lazy: () => import("./Pages/HomePage/HomePage.tsx"),
+              path: "not-found",
+              lazy: () => import("./components/NotFound.tsx"),
             },
           ],
         },
-        {
-          path: "not-found",
-          lazy: () => import("./components/NotFound.tsx"),
-        },
       ],
-    },
-    //!NotFound
-    {
-      path: "*",
-      lazy: () => import("./components/NotFound.tsx"),
     },
   ]);
 
   return <RouterProvider router={router} />;
+  /*   return <RouterProvider router={router} fallbackElement={< Loader />} />;
+   */
 }
 
 export default App;
