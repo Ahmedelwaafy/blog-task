c7597eb5fd6b1b58939cfc3752929aa8f1ac5bae
Ahmedelwaafy <ahmedelwaafy@gmail.com>
2024-04-22 05:14:58 +0200

    deploy edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 function Footer() {
   const { t } = useTranslation("HomePage");
 
-  return <footer className="h-10 bg-accent w-full flex-center text-background">{t("footer")}</footer>;
+  return <footer className="h-10 bg-accent w-full flex-center text-background mt-10">{t("footer")}</footer>;
 }
 
 export default Footer;
